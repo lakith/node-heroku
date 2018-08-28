cc7fb7f05d1df4f20f0d7864d688148bdae5ba9d
lakith <lakith1995@gmail.com>
2018-08-28 16:24:48 +0530

    completing crup and deplying heroku

--- controller/employeeControleller.js
+++ controller/employeeControleller.js
@@ -1,4 +1,5 @@
-let express = require('express');
+const _ = require('lodash');
+const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
@@ -44,7 +45,7 @@ app.get('/employee/:id',(req,res)=>{
     let id = req.params.id;
 
     if(!ObjectID.isValid(id)){
-        return res.status(404).send({message:"Invalied id"});
+        return res.status(400).send({message:"Invalied id"});
     }
     employee.findById(id).then((docs)=>{
         if(!docs){
@@ -55,6 +56,47 @@ app.get('/employee/:id',(req,res)=>{
         res.send(err);
     });
 });
+
+app.delete('/employee/:id',(req,res)=>{
+
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send({message:"Invalied id"});
+    }
+
+    employee.findByIdAndRemove(id).then((docs)=>{
+
+        if(!docs){
+            return res.status(400).send({message:"Invalied id"});
+        }
+
+        res.send(docs);
+    },(err)=>{
+        res.send(err);
+    })
+
+});
+
+app.patch('/employee/:id',(req,res)=>{
+    let id = req.params.id;
+    var body = _.pick(req.body,['name','age']);
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send('Invalied id');
+    }
+
+    employee.findByIdAndUpdate(id,{$set: body}, {new:true}).then((docs)=>{
+        if(!docs){
+            return res.status(400).send({message:"Invalied id"});
+        }
+        res.send({docs});
+    }).catch((e)=>{
+        res.status(400).send(e)
+    });
+    
+})
+
+
 
 app.listen (PORT || 3000,()=>{
     console.log(`server started on port $ `);
