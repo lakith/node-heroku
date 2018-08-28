d87429360b7f113330018ea4713ac286883bc3b8
lakith <lakith1995@gmail.com>
2018-08-28 13:20:22 +0530

    adding environment variables

--- DB/mongo_connection.js
+++ DB/mongo_connection.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/DB_resturent",{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/DB_resturent",{ useNewUrlParser: true });
 
 module.exports = {mongoose};
